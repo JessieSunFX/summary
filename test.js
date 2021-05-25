41cccae4fae079c1da5b6ae5066c14ffc60c57f4
jessie <15611006491@163.com>
2021-05-25 17:49:51 +0800

    888

--- test.js
+++ test.js
@@ -68,30 +68,52 @@ Object.defineProperty(obj,key,{
     if(!nums || nums.length < 3) return [];
     let result = [], second, last;
     nums.sort((a, b) => a - b);
-    for(i = 0; i < nums.length - 2; i++) {
+    for(let i = 0; i < nums.length - 2; i++) {
         if(nums[i] > 0) break;
         if(i > 0 && nums[i] === nums[i - 1]) continue;
         second = i + 1;
         last = nums.length - 1;
-        while(second < last){
+        while(second < last) {
             const sum = nums[i] + nums[second] + nums[last];
-            if(!sum) {
+            if(!sum){
                 result.push([nums[i], nums[second], nums[last]]);
-                
-                while(second < last && nums[second] === nums[second + 1]) second ++;
-                while(second < last && nums[last] === nums[last - 1]) last --;
-                
-                seonde++;
-                last--;
-            } else if(sum < 0) {
+
+                while(second < last && nums[second] === nums[second + 1]) second++;
+                while(second < last && nums[last] === nums[last - 1]) last--;
+
                 second++;
-            } else if(sum > 0) {
                 last--;
             }
+            else if(sum < 0) second++;
+            else if(sum > 0) last--;
         }
     }
     return result;
  }
 
- 
+ var threeSum = function(nums) {
+    if(!nums || nums.length < 3) return [];
+    let result = [], second, last;
+    nums.sort((a, b) => a -b);
+    for(let i = 0; i < nums.length - 2; i++) {
+        if(nums[i] > 0) break;
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+        second = i + 1;
+        last = nums.length - 1;
+        while(second < last) {
+            const sum = nums[i] + nums[second] + nums[last];
+            if(!sum) {
+                result.push([nums[i], nums[second], nums[last]]);
 
+                while(second < last && nums[second] === nums[second + 1]) second++;
+                while(second < last && nums[last] === nums[last - 1]) last--;
+
+                second++;
+                last--;
+            }
+            else if(sum < 0) second++;
+            else if(sum > 0) last--;
+        }
+    }
+    return result;
+ }
